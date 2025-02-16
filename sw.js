2e86205d76730e261ca2db5bbd2c6d8770369766
Andrei Shabanski <shabanski.andrei@gmail.com>
2025-02-16 21:09:19 +0200

    feat: cache requests to aws lambda

--- sw.js
+++ sw.js
@@ -19,7 +19,8 @@ self.addEventListener('fetch', event => {
     if (
         event.request.url.startsWith(self.location.origin) || 
         event.request.url.match(/fonts.(googleapis|gstatic).com/) || 
-        event.request.url.match(/dict.api.pgaskin.net/)
+        event.request.url.match(/dict.api.pgaskin.net/) ||
+        event.request.url.match(/on.aws/)
 /* Cache first: */
     ) event.respondWith(
         caches.open(`${cachePrefix}-${revision}`).then(
